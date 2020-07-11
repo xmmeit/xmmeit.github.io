19be2cfa5fd584ffa4e2d1a338e68191924b96bb
Xavier Meitner <68140410+xmmeit@users.noreply.github.com>
2020-07-10 23:43:51 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <title>Ismail Tasdelen</title>
-  <meta content="security researcher, security consultant, bug bounty, bug bounty hunter, web security, penetration testing, cyber security, information security, siber güvenlik, bilgi güvenliği" name="keywords">
-  <meta content="Security Consultant" name="description">
+  <meta content="ml researcher, market research consultant, instructor, faculty, scientist" name="keywords">
+  <meta content="Founder" name="description">
   <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
   <link href="assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/lib/ionicons/css/ionicons.min.css" rel="stylesheet">
@@ -20,15 +20,15 @@
   <div id="header" class="home">
     <div class="container">	
       <div class="header-content">
-        <h1>I'm <span class="typed"></span></h1>
-        <p>security researcher, bug bounty hunter, developer</p>
+        <h1>I'm a <span class="typed"></span></h1>
+        <p>Harvard alum, Berkeley grad, ex-Jane Street, ex-Google</p>
         <ul class="list-unstyled list-social">
-		  <li><a href="https://github.com/ismailtasdelen" target="_blank"><i class="ion-social-github"></i></a></li>
-          <li><a href="https://www.linkedin.com/in/ismailtasdelen/" target="_blank"><i class="ion-social-linkedin" target="_blank"></i></a></li>
-          <li><a href="https://twitter.com/@ismailtsdln" target="_blank"><i class="ion-social-twitter"></i></a></li>
-          <li><a href="https://www.youtube.com/channel/UCPqc50Wmt6ZF2BJWtNJlq-g"><i class="ion-social-youtube" target="_blank"></i></a></li>
-		  <li><a href="https://www.instagram.com/mr.ismailtasdelen/" target="_blank"><i class="ion-social-instagram"></i></a></li>
-		  <li><a href="https://www.facebook.com/mr.ismailtasdelen" target="_blank"><i class="ion-social-facebook"></i></a></li>
+		  <li><a href="https://github.com/xmmeit" target="_blank"><i class="ion-social-github"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/xavier-marcedo/" target="_blank"><i class="ion-social-linkedin" target="_blank"></i></a></li>
+          <li><a href="https://twitter.com/" target="_blank"><i class="ion-social-twitter"></i></a></li>
+          <li><a href="https://www.youtube.com/"><i class="ion-social-youtube" target="_blank"></i></a></li>
+		  <li><a href="https://www.instagram.com/xmmeitner/" target="_blank"><i class="ion-social-instagram"></i></a></li>
+		  <li><a href="https://www.facebook.com/xavier.meitner.7" target="_blank"><i class="ion-social-facebook"></i></a></li>
       </div>
     </div>
   </div>
